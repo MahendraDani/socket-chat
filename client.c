4d9a80670734f302e9ff697969f270b671de8e9d
Mahendra Dani <danimahendra0904@gmail.com>
2025-02-07 16:33:01 +0530

    client-server v0

---
+++ client.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>  // For close()
+
+int main() {
+    int socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0) {
+        perror("Socket creation failed");
+        return EXIT_FAILURE;
+    }
+
+    struct sockaddr_in address;  // Correct structure for IPv4 sockets
+    memset(&address, 0, sizeof(address)); // Zero out the structure
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(2000);  // Port of the server
+
+    char* ip = "127.0.0.1"; // IP Address of Google
+    if (inet_pton(AF_INET, ip, &address.sin_addr) <= 0) {
+        perror("Invalid IP address");
+        close(socketFD);
+        return EXIT_FAILURE;
+    }
+
+    int result = connect(socketFD, (struct sockaddr*)&address, sizeof(address));
+    if(result !=0){
+      perror("Connection failed");
+      return EXIT_FAILURE;
+    }
+
+    printf("Connection was successful\n");
+
+    char* message = "Hi, there! I am the client";
+    send(socketFD,message,strlen(message),0);
+
+    char buffer[1024];
+
+    recv(socketFD,buffer,1024,0);
+    printf("%s\n",buffer);
+
+    close(socketFD);
+    return 0;
+}
---
+++ server.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+struct sockaddr_in* createIPv4Address(char *ip, int port) {
+  struct sockaddr_in  *address = malloc(sizeof(struct sockaddr_in));
+  address->sin_family = AF_INET;
+  address->sin_port = htons(port);
+
+  if(strlen(ip) ==0)
+      address->sin_addr.s_addr = INADDR_ANY;
+  else
+      inet_pton(AF_INET,ip,&address->sin_addr.s_addr);
+
+  return address;
+}
+
+int main(){
+  int serverSocketFD = socket(AF_INET, SOCK_STREAM, 0);
+  if (serverSocketFD < 0) {
+    perror("Socket creation failed");
+    return EXIT_FAILURE;
+  }
+
+  struct sockaddr_in *serverAddress = createIPv4Address("",2000);
+
+  int result = bind(serverSocketFD,(struct sockaddr*)serverAddress, sizeof(*serverAddress));
+
+  if(result !=0){
+    perror("Server Socket was bind failure\n");
+    return EXIT_FAILURE;
+  }
+
+  printf("Server Socket bound successfully\n");
+
+  int listenResult = listen(serverSocketFD,10);
+
+  struct sockaddr_in clientAddress;
+  
+  int clientAddressSize = sizeof(struct sockaddr_in);
+  int clientSocketFD = accept(serverSocketFD,clientSocketFD,&clientAddressSize);
+
+  char buffer[1024];
+  recv(clientSocketFD,buffer,1024,0);
+  
+  char* message = "Hi, there! I am the server";
+  send(clientSocketFD,message,strlen(message),0);
+
+  printf("Response was %s\n",buffer);
+
+  return 0;
+}
